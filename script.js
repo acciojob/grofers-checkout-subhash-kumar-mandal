550136cd369619d855e4ec84cc084398b2b72b55
subhash kumar <pm817405@gmail.com>
2026-01-30 11:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let price = document.querySelectorAll('.price');
+	let total = 0;
+	price.forEach(price=>total+= Number(price.innerText));
+
+    let table = document.querySelector('table');
+	let rownew = document.createElement('tr');
+	
+	let newcell = document.createElement('td');
+
+	newcell.colSpan =2;
+	newcell.innerText= "Total" +total;
+	rownew.appendChild(newcell);
+	table.appendChild(rownew)
+	
   
 };
 
